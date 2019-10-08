0d77e6063be3073571bcaa432ab1a1b418f5ccbe
gdq <fkljds@fafa.com>
2019-10-08 09:48:29 +0800

    xxxx

--- js/activity.js
+++ js/activity.js
@@ -1,4 +1,4 @@
-var homes=[
+var activitys = [
     "长官,GDQ向你报道",
     "[天猫]告别”不合身“属于我们自己的...",
     "红色军魂节系列活动激情来袭",
@@ -10,11 +10,11 @@ var homes=[
     "《战舰世界》玩家触媒习惯有奖调查问卷",
     "我是最靓的仔----GDQ"
 ];
-var newArr = homes.map(
-    function(elem){
-        return `<li><b id="zx1">[新闻]</b><a href=""><span id="zx2">${elem}</span></a><b id="zx4">08-17</b></li>`
+var newArr = activitys.map(
+    function (elem) {
+        return `<li><b id="zx1">[活动]</b><a href=""><span id="zx2">${elem}</span></a><b id="zx4">08-17</b></li>`
     }
 )
 console.log(newArr);
-var html=newArr.join("");
-home.innerHTML=html;
+var html = newArr.join("");
+activity.innerHTML = html;
